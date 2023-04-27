4613361c21c770888ecc8233f42c96a7f53d7f62
soroel <sorophinenoel@gmail.com>
2023-04-27 14:04:12 +0300

    my first flutter app

---
+++ lib/chart_screen.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+
+class ChatScreen extends StatefulWidget {
+  final String friendName;
+  final int friendIndex;
+
+  const ChatScreen({
+    Key? key,
+    required this.friendName,
+    required this.friendIndex,
+  }) : super(key: key);
+
+  @override
+  _ChatScreenState createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  final TextEditingController _textEditingController = TextEditingController();
+
+  final List<String> _messages = [];
+
+  void _sendMessage() {
+    if (_textEditingController.text.isNotEmpty) {
+      setState(() {
+        _messages.add(_textEditingController.text);
+        _textEditingController.clear();
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.friendName),
+        backgroundColor: Colors.deepPurpleAccent,
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              reverse: true,
+              itemCount: _messages.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(_messages[index]),
+                  trailing: Icon(Icons.message),
+                );
+              },
+            ),
+          ),
+          Row(
+            children: [
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextField(
+                    controller: _textEditingController,
+                    decoration: const InputDecoration(
+                      hintText: "Type your message",
+                    ),
+                  ),
+                ),
+              ),
+              IconButton(
+                icon: const Icon(Icons.send),
+                onPressed: _sendMessage,
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
---
+++ lib/main.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+import 'chart_screen.dart';
+
+void main() => runApp(const ChattingApp());
+
+class ChattingApp extends StatelessWidget {
+  const ChattingApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+     debugShowCheckedModeBanner: false,
+      title: 'Chat With Me',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Hello App'),
+          titleTextStyle: const TextStyle(color: Colors.black),
+          backgroundColor: Colors.deepPurpleAccent,
+
+
+
+        ),
+
+        body: Column(
+          children: <Widget>[
+            Expanded(
+              child: ListView.builder(
+                itemCount: 10,
+                itemBuilder: (BuildContext context, int index) {
+// get friend's index and name
+                  final friendIndex = index + 1;
+                  final friendName = 'Friend $friendIndex';
+
+                  return GestureDetector(
+                    onTap: () {
+// navigate to chat screen
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (_) => ChatScreen(
+                            friendName: friendName,
+                            friendIndex: friendIndex,
+                          ),
+                        ),
+                      );
+                    },
+                    child: ListTile(
+                      title: Text(friendName),
+                      subtitle: const Text('This is the latest message'),
+                      leading: CircleAvatar(
+                        backgroundColor: Colors.deepPurpleAccent,
+                        child: Text('U$friendIndex'),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+
+
+          ],
+        ),
+      ),
+    );
+  }
+}
